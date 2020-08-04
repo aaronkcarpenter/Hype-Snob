7cac84caf5fee3b9164f76861d96f903e44ef8ba
Aaron <aaronkcarpenter@gmail.com>
2020-08-04 15:51:00 -0500

    Final heroku edits

--- backend/app.js
+++ backend/app.js
@@ -27,6 +27,13 @@ app.use(signUp);
 app.use(style);
 app.use(wantList);
 
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+  app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
 
 // Catch unhandled requests and forward to error handler.
 app.use((req, res, next) => {
